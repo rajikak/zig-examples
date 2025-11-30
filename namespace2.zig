cc04fe9e8c862c932a567539a88774e771728811
EC2 Default User <nrajikak@gmail.com>
2025-11-30 03:22:26 +0000

    add namespace

--- namespace2.zig
+++ namespace2.zig
@@ -3,7 +3,8 @@ const linux = std.os.linux;
 
 const c = @cImport({
     @cInclude("unistd.h");
-    @cInclude("stdlib.h");
+    @cDefine("_GNU_SOURCE", {});
+    @cInclude("sched.h");
 });
 
 const Arg = struct {
@@ -19,24 +20,6 @@ fn child(arg: usize) callconv(.c) u8 {
 
     std.debug.print("[child]  tid={d}, pid={d}, ppid={d}, buf={s}\n", .{ linux.gettid(), linux.getpid(), linux.getppid(), input.buf });
 
-    return 0;
-}
-
-pub fn toPid(x: usize) !linux.pid_t {
-    if (x > std.math.maxInt(u64)) return error.PidOutOfRange;
-    return @intCast(x);
-}
-
-pub fn main() !void {
-    //const stack_size: usize = 1024 * 1024;
-    //const stack_memory = try std.heap.page_allocator.alloc(u8, stack_size);
-    //defer std.heap.page_allocator.free(stack_memory);
-
-    //const stack_ptr = @intFromPtr(stack_memory.ptr + stack_size);
-    //const clone_flags = linux.CLONE.NEWUTS | linux.SIG.CHLD | linux.CLONE.VM;
-
-    //const arg = Arg.init("nshostname.org"); // set a sample hostname
-
     var buffer: [std.posix.HOST_NAME_MAX]u8 = undefined;
     const res = try std.posix.gethostname(&buffer);
     const uts = std.posix.uname();
@@ -46,10 +29,27 @@ pub fn main() !void {
     const rc = c.sethostname(host, host.len);
     if (rc != 0) {
         const err = std.posix.errno(rc);
-        std.debug.print("[error]sethostname failed, error={}, {any}\n", .{ err, rc });
-        return;
+        std.debug.print("[error]sethostname failed, error={}\n", .{err});
     }
-    std.debug.print("hostname={s}, uts={s}\n", .{ res, uts.nodename });
+
+    return 0;
+}
+
+pub fn toPid(x: usize) !linux.pid_t {
+    if (x > std.math.maxInt(u64)) return error.PidOutOfRange;
+    return @intCast(x);
+}
+
+pub fn main() !void {
+    const stack_size: usize = 1024 * 1024;
+    const stack_memory = try std.heap.page_allocator.alloc(u8, stack_size);
+    defer std.heap.page_allocator.free(stack_memory);
+
+    const stack_ptr = @intFromPtr(stack_memory.ptr + stack_size);
+    const clone_flags = linux.CLONE.NEWUTS | linux.SIG.CHLD | linux.CLONE.VM;
+
+    const res = c.clone(child, stack_ptr, clone_flags, null, 0, null);
+    _ = res;
 
     //std.debug.print("[parent] tid={d}, pid={d}, ppid={d}, clone={d}\n", .{ linux.gettid(), linux.getpid(), linux.getppid(), pid });
 
