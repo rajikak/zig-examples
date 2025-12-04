d6ece136c98def935319a87d6f6a02e676d9f854
EC2 Default User <nrajikak@gmail.com>
2025-12-04 01:10:39 +0000

    add format functions

--- format.zig
+++ format.zig
@@ -3,21 +3,17 @@ const linux = std.os.linux;
 
 const buf_size = 100;
 
-fn log(s: []const u8) !void {
+fn log(comptime s: []const u8) !void {
     var buf: [buf_size]u8 = undefined;
     var w = std.fs.File.stdout().writer(&buf);
     const stdout = &w.interface;
 
-    stdout.print(s);
-    stdout.flush();
+    try stdout.print(s, .{});
+    try stdout.flush();
 }
 
 pub fn main() !void {
-    
-    
-    var buf:[buf_size]u8 = undefined;
+    var buf: [buf_size]u8 = undefined;
     const a = try std.fmt.bufPrint(&buf, "[parent]: pid = {d}\n", .{linux.getpid()});
-
-    log()
-
+    try log(a);
 }
